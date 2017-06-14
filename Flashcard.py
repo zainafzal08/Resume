63940073efb69d6931506174e813f7ad9bb351c9
Zain Afzal <zain.afz@gmail.com>
2017-06-14 13:22:14 +1000

    Updated flashcards to allow for tables and multi-line flashcards

--- Flashcard.py
+++ Flashcard.py
@@ -4,6 +4,7 @@ import sys
 import random
 from flask import Flask, render_template, request, g, session, redirect
 import time
+import re
 
 def handleRequest(request):
 	if request.method == "GET":
@@ -12,28 +13,96 @@ def handleRequest(request):
 	# get the raw data
 	title = request.form['title']
 	inputText = request.form['inputText']
+	# sanitise
+	inputText = re.sub(r'\<', r'&gt;', inputText);
+	inputText = re.sub(r'\>', r'&lt;', inputText);
+	inputText = re.sub(r'\r', r'', inputText);
 	# process
 	flashcards = getFlashcards(inputText)
 	# render
 	return render_template("flashcards.html", tag=str(time.time()), title=title, flashcards=flashcards)
 
-def getFlashcards(inputText):
-	flashcards = []
-	count = 1
-	question = "ERROR"
-	inputTextLines = inputText.split("\n")
-	for i,line in enumerate(inputTextLines):
-		if(count % 3 == 0):
-			count = 1
-			continue
-		elif(count % 2 == 0):
-			flashcards.append((question, line.strip(), i))
-		else:
-			question = line.strip();
-		count+=1
 
+def multiSplit(text, sep):
+	l = len(sep)
+	currCount = 0
+	result = []
+	buffer = []
+	for c in text:
+		if(c == sep[currCount]):
+			currCount+=1
+			if(currCount == l):
+				end = len(buffer) - l
+				result.append("".join(buffer[0:end]))
+				buffer = []
+				currCount = 0
+		else:
+			currCount = 0
+		buffer.append(c)
+	if(len(buffer) > 0):
+		result.append("".join(buffer))
+	return result
+# must return an array of question, answer, string tuples
+def getFlashcards(inputText):
+	cardSeperator = "\n\n"
+	QASeperator = "\n---\n"
+	rawCards = inputText.split(cardSeperator)
+	flashcards = []
+	for i,rawCard in enumerate(rawCards):
+		bits = rawCard.split(QASeperator)
+		question = compile(bits[0])
+		answer = compile(bits[1])
+		flashcards.append((question, answer, i))
+	# shuffle and return
 	random.shuffle(flashcards)
 	return flashcards
 
+# inserts a table into list of lines
+def insertTable(lines, table, index):
+	for i,l in enumerate(lines):
+		if l == "!table["+str(index)+"]":
+			lines[i] = table
+			return
+	raise Exception("Index not Found")
 
+def compileTable(rawTable):
+	final = []
+	final.append("<br><table class='table table-striped table-hover'>")
+	final.append("  <tbody>")
+	for line in rawTable:
+		final.append("    <tr>")
+		cleanLine = re.sub(r'\s+',r' ', line)
+		for data in cleanLine.split(" "):
+			final.append("      <td>"+data+"</td>")
+		final.append("    </tr>")
+	final.append("  </tbody>")
+	final.append("</table>")
+	return "\n".join(final)
 
+# compiles tables and the such
+def compile(text):
+	currTable = -1
+	result = []
+	tables = []
+	inTable = False
+	# get base positioning and line seperation
+	for line in text.split('\n'):
+		if(line == "!endtable"):
+			inTable = False
+			continue
+		if(line == "!table"):
+			inTable = True
+			currTable+=1
+			tables.append([])
+			result.append(line+"["+str(currTable)+"]")
+			continue
+		if(inTable):
+			tables[currTable].append(line)
+		else:
+			result.append(line)
+	# now compile down tables
+	for index, table in enumerate(tables):
+		t = compileTable(table)
+		insertTable(result, t, index)
+	#remember to join with "<br>"
+	return "<br>".join(result)
