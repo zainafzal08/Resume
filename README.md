a312c1444f3dab172f926eb09d0aad4fbcee3ef6
Zain Afzal <zain.afz@gmail.com>
2018-01-12 09:17:02 +1100

    Added basic card content + made animations work with them

--- README.md
+++ README.md
@@ -9,6 +9,14 @@ z index seperates poster images, as you scroll it flips through them, each one w
 keep to pure github atm, the live sites need work yikes.
 keep same text element at highest z index, animate it clearing and retyping on scroll. (pretty quickily)
 
+## pages
+
+page on multipel web escarping scripts
+page on sec seoc stuff
+rest copy
+maybe chuck in adventure buddy?
+nodeboard?
+check out github i dunno lol. Maybe chuck in svg stuff with a overall web page
 
 ## Build Setup
 
--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,13 +1,34 @@
 <template>
   <div class="poster-container">
-    <div id ="card-1" class="poster-card" :style="{background: '#444444'}">
+    <div id ="card-1" class="poster-card poster-1 shadow">
+      <div class="innards" id="card-1-innards">
+        <div class="card-header">
 
+        </div>
+        <div class="card-content">
+          <div class="center-text">
+            <h1> Zain Afzal </h1>
+            <hr>
+            <div class="logos">
+
+            </div>
+            <h3> He's alright i guess </h3>
+          </div>
+        </div>
+        <div class="card-footer">
+
+        </div>
+      </div>
     </div>
-    <div id ="card-2" class="poster-card" :style="{background: '#00FF00'}">
+    <div id ="card-2" class="poster-card poster-2 shadow">
+      <div class="innards" id="card-2-innards">
 
+      </div>
     </div>
-    <div id ="card-3" class="poster-card" :style="{background: '#0000FF'}">
+    <div id ="card-3" class="poster-card poster-3 shadow">
+      <div class="innards" id="card-3-innards">
 
+      </div>
     </div>
   </div>
 </template>
@@ -44,11 +65,17 @@ export default {
     },
     nextCard () {
       window.Velocity(document.getElementById('card-' + this.card), {height: '0vh'}, 800)
+      window.Velocity(document.getElementById('card-' + this.card + '-innards'), {opacity: '0'}, 800)
       this.card++
     },
     prevCard () {
+      window.Velocity(document.getElementById('card-' + this.card + '-innards'), {opacity: '1'}, 800)
       this.card--
       window.Velocity(document.getElementById('card-' + this.card), {height: '100vh'}, 800)
+      window.Velocity(document.getElementById('card-' + this.card + '-innards'), {opacity: '1'}, 800)
+    },
+    jumpCard (c) {
+
     }
   },
   created () {
@@ -64,6 +91,7 @@ export default {
 <style scoped>
   @import url('https://fonts.googleapis.com/css?family=Roboto|Source+Sans+Pro|Ubuntu');
 
+  /* magic to capture all scroll events */
   .poster-container {
     width: 100vw;
     height: 100vh;
@@ -74,12 +102,58 @@ export default {
     width: 100vw;
     height: 100vh;
   }
-  /*background-image: url('./images/poster_1.jpeg');
-  background-repeat: no-repeat;
-  background-size: cover;
-  position: absolute;*/
 
-  /* from material design */
+  /* actual card components */
+  .poster-card .card-header{
+    width: 100%;
+    height: 10%;
+    display: flex;
+  }
+  .poster-card .card-content{
+    width: 100%;
+    height: 70%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-family:  'Ubuntu', sans-serif;
+    color: white;
+  }
+  .poster-card .card-content h1{
+    font-size: 3rem;
+  }
+  .poster-card .card-footer{
+    width: 100%;
+    height: 20%;
+    display: flex;
+  }
+
+  .poster-card .logos{
+    text-align: center;
+    width: 50%;
+  }
+
+  .poster-card .center-text{
+    text-align: center;
+    width: 50%;
+  }
+  .poster-card .innards{
+    width: 100%;
+    height: 100%;
+  }
+  /* poster background info */
+  .poster-1 {
+    background: #444444;
+  }
+  .poster-2 {
+    background-image: url('./images/poster_1.jpeg');
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+  .poster-3 {
+    background: #EBEBEB;
+  }
+
+  /* material design shadows */
   .shadow {
     box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
     transition: all 0.3s cubic-bezier(.25,.8,.25,1);
