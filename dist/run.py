910990ff5e684377518dfcfb7958cfb572baac9a
Zain Afzal <zain.afz@gmail.com>
2018-02-03 23:50:22 +1100

    Added in image grabber api

--- dist/run.py
+++ dist/run.py
@@ -1,3 +1,6 @@
+import pexelScraper
+import json
+
 from flask import Flask, render_template, request, g, session, redirect
 app = Flask(__name__)
 app.secret_key = 'B3Dvm1BJF1'
@@ -5,6 +8,19 @@ app.secret_key = 'B3Dvm1BJF1'
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/imgRequest')
+def imgRequest():
+	try:
+		w = int(request.args['w'])
+		h = int(request.args['h'])
+	except:
+		abort(404)
+	img = pexelScraper.simpleSearch(w,h)
+	if img != None:
+		return json.dumps(img)
+	else:
+		abort(404)
 
 if __name__ == "__main__":
 	app.run(debug=False)
